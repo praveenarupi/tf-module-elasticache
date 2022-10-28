90d9033cb61c0d7b12117a7ae7d461187cf58be9
praveenarupi <praveenarupi@gmail.com>
2022-10-28 11:54:32 +0530

    updating ./terraform-modules/tf-module-elasticache

--- main.tf
+++ main.tf
@@ -34,6 +34,12 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr_block]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [0.0.0.0/0]
+  }
   tags = {
     Name = "${var.env}-redis"
   }
